5b3c349b3689e13f2dc25e975ace2ef056b4ecd3
designdrop <abekalili@gmail.com>
2015-07-09 18:50:18 +0200

    Preloaded seems to work
    Thanks to all the ones who share their knowledge

--- catalist-credits/js/main.js
+++ catalist-credits/js/main.js
@@ -3,25 +3,11 @@ $(document).ready(function() {
 	setTimeout(function(){
 		$('body').addClass('loaded');
 		$('h1').css('color','#222222');
-	}, 3000);
+	});
     
-    //        var s = skrollr.init();
-    //        smoothScrolling: false,
-    //$(window).load(function(){
-    //    console.log("Loaded");
-    //    
-    //    setTimeout(function () {
-    //  skrollr.get().refresh();
-    //}, 0);
-    //            
-    //    $(window).trigger('resize');
-    //    s.refresh();
-    //});
-    //       
-
     var s = skrollr.init({
         //    scale: 5,
-        smoothScrolling: false,
+//        smoothScrolling: false,
 
         render: function(data) {
             //Log the current scroll position.
